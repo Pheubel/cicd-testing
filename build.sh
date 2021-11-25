dd2d81f862fc613a8b462dceef4aaa18a937c1ce
Pheubel <luca.guagliardo@live.nl>
2021-11-25 18:27:16 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,30 +4,3 @@ current_dir=$PWD
 
 script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
-cd "$script_dir"
-
-sp_executable="$1"
-out_path="$script_dir/build"
-include_paths=""
-sp_optimize="-O2"
-sp_verbose="-v2"
-
-for a in "${@:2}"
-do
-    case $a in -i=*)
-        include_paths="$include_paths $a"
-    esac
-done
-
-for script in "${@:2}"
-do
-    case $script in *sp)
-        script_name="${script%.*}"
-        script_name="${script_name##*/}"
-        outArg="-o=$script_name.smx"
-
-        echo $outArg
-        
-        "$sp_executable" $script "$outArg" $include_paths $sp_optimize $sp_verbose
-    esac
-done
